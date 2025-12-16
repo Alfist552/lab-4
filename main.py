9221fb9d870716ba94adb0a1d31a34cca89309ea
Alfist552 <ndetsura@mail.ru>
2025-12-16 18:10:13 +0300

    Добавление start_command и help_command. На данном этапе бот выводит при помощи "/start" информацию о том кто он и что может делать, а при помощи "/help" бот показывает список всех возможных комманд

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from aiogram import Bot,Dispatcher, types, executor
 
 logging.basicConfig(
     level = logging.INFO,
@@ -7,5 +8,40 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 API_TOKEN = ''
-
 URL_OMDb_TOKEN = ''
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    try:
+        text = """Я - Киношник, бот для помощи поиска фильмов
+    Я могу найти:
+    * Найти информацию о фильмах
+    * Показать его рейтинг
+    * Дать краткое описание
+
+Напишите /help для вывода всех команд"""
+        await message.answer(text)
+
+    except Exception as e:
+        logger.error(f"Ошибка:{e}")
+        await message.answer("Ошибка, попробуйте еще раз")
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    try:
+        text ="""Список всех команд:
+    /search - поиск фильма
+    /info - информация о источнике данных
+    /start - главное меню"""
+        await message.answer(text)
+
+    except Exception as e:
+        logger.error(f"Ошибка в /help: {e}")
+        await message.answer("Произошла ошибка")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
